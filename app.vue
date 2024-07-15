761867219c7f585465737872234344891b1e0d82
KyleSmith0905 <kylesmith090502@gmail.com>
2024-07-15 17:23:59 -0400

    first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+useSchemaOrg([
+	defineVideo({
+		name: 'Title of video',
+		description: 'Description of video',
+		url: 'https://yskkyle.com',
+		thumbnailUrl: {url: 'https://yskkyle.com/icons/logo192.png'},
+		uploadDate: new Date(),
+	})
+])
+</script>
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
